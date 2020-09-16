319c4263e33768a64ea3eaf081174caf26c44ab2
suay <suaynaruk_92@hotmail.com>
2020-09-16 12:00:20 +0700

    test

---
+++ rpt_user_202009161124.sql
@@ -0,0 +1,8 @@
+INSERT INTO rptsample.rpt_user (firstName,lastName,email,email_verified_at,password,rememberToken,phone,typeUser,employeeCard,`position`,jobDuty,groupType,createdAt,updatedAt,createBy,updateBy) VALUES 
+('คนทดสอบ',NULL,'test@email.com',NULL,'$2y$10$/SeMx0RHrQZfRDM32KymkeNNMBHe/jOz0chuBVPWC4k8Gtu68HsDC',NULL,NULL,NULL,NULL,NULL,NULL,0,'2020-09-02 04:56:43.000','2020-09-11 16:25:06.000',NULL,NULL)
+,('suay',NULL,'suaynaruk_92@hotmail.com',NULL,'$2y$10$/SeMx0RHrQZfRDM32KymkeNNMBHe/jOz0chuBVPWC4k8Gtu68HsDC',NULL,NULL,NULL,NULL,NULL,NULL,0,'2020-09-08 07:08:42.000','2020-09-11 14:20:00.000',NULL,NULL)
+,('tier1',NULL,'tier1@gmail.com',NULL,'$2y$10$2TRm7W9glNIMmxS3pKfjteBcLfynL2H8mq9V1iDcxQway.8jrT49G',NULL,NULL,NULL,NULL,NULL,NULL,1,'2020-09-10 10:02:00.000','2020-09-11 14:20:00.000',NULL,NULL)
+,('tier22',NULL,'tier12@gmail.com',NULL,'$2y$10$qO..Fgngvae0vdUSv.6Bq.vV8eysGmIuQw16agmW8o8zpCNHWz4dy',NULL,NULL,NULL,NULL,NULL,NULL,2,'2020-09-11 02:09:42.000','2020-09-15 11:17:25.000',NULL,NULL)
+,('usertest1',NULL,'usertest1@gmail.com',NULL,'$2y$10$M/4dGAOJPB7oMMpg0aoCrOgf.94CZPxssP2TGBMYAIBBpm.d.hNe6',NULL,NULL,NULL,NULL,NULL,NULL,0,'2020-09-11 06:59:31.000','2020-09-11 14:20:00.000',NULL,NULL)
+,('admin',NULL,'admin@email.com',NULL,'$2y$10$/SeMx0RHrQZfRDM32KymkeNNMBHe/jOz0chuBVPWC4k8Gtu68HsDC',NULL,NULL,'0',NULL,NULL,NULL,0,'2020-09-14 11:09:40.000',NULL,NULL,NULL)
+;
